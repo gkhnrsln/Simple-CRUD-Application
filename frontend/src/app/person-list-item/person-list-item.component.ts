77e647b693d23cc965a54fe78568308e1d971711
Gökhan Arslan <goekhan.arslan@outlook.de>
2025-03-02 00:16:59 +0100

    add loggedin only directive to show edit and delete button only if logged in

--- frontend/src/app/person-list-item/person-list-item.component.ts
+++ frontend/src/app/person-list-item/person-list-item.component.ts
@@ -3,10 +3,11 @@ import { Person } from '../model/person';
 import { DatePipe } from '@angular/common';
 import { PersonService } from '../shared/service/person.service';
 import { RouterLink } from '@angular/router';
+import { LoggedinOnlyDirective } from '../shared/loggedin-only.directive';
 
 @Component({
   selector: '[app-person-list-item]',
-  imports: [DatePipe, RouterLink],
+  imports: [DatePipe, RouterLink, LoggedinOnlyDirective],
   templateUrl: './person-list-item.component.html',
   styleUrl: './person-list-item.component.scss',
 
