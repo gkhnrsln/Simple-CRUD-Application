dc1c27f4bd1bf12828844c0d8d18855df0febc91
Gökhan Arslan <goekhan.arslan@outlook.de>
2025-03-03 15:35:21 +0100

    fix personform not loading values from person

--- frontend/src/app/admin/person-form/person-form.component.ts
+++ frontend/src/app/admin/person-form/person-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Person } from 'src/app/model/person';
 import { birthdayValidator } from 'src/app/shared/validators/birthdayValidator';
@@ -12,44 +12,41 @@ import { birthdayValidator } from 'src/app/shared/validators/birthdayValidator';
       ReactiveFormsModule
     ]
 })
-export class PersonFormComponent implements OnInit {
+export class PersonFormComponent {
   @Input() person?: Person;
   @Output() submitPerson = new EventEmitter<Person>();
-  personForm!: FormGroup;
-  
+
   ngOnChanges() :void {
     if (this.person) {
       this.setFormValues(this.person);
     }
   }
 
-  ngOnInit(): void {
-    this.personForm = new FormGroup({
-      firstName: new FormControl('John', {
-        nonNullable: true,
-        validators: Validators.required
-      }),
-      lastName: new FormControl('Doe', {
-        nonNullable: true,
-        validators: Validators.required
-      }),
-      birthday: new FormControl(new Date(), {
-        nonNullable: true,
-        validators: [
-          Validators.required, 
-          birthdayValidator()
-        ]
-      }),
-      mail: new FormControl('example@abc.com', {
-        nonNullable: true,
-        validators: Validators.email
-      }),
-      phone: new FormControl('+0123456789', {
-        nonNullable: true
-      }),
-    });
-  }
-
+  personForm = new FormGroup({
+    firstName: new FormControl('John', {
+      nonNullable: true,
+      validators: Validators.required
+    }),
+    lastName: new FormControl('Doe', {
+      nonNullable: true,
+      validators: Validators.required
+    }),
+    birthday: new FormControl(new Date(), {
+      nonNullable: true,
+      validators: [
+        Validators.required, 
+        birthdayValidator()
+      ]
+    }),
+    mail: new FormControl('example@abc.com', {
+      nonNullable: true,
+      validators: Validators.email
+    }),
+    phone: new FormControl('+0123456789', {
+      nonNullable: true
+    }),
+  });
+  
   get firstName() {
     return this.personForm.get('firstName');
   }
