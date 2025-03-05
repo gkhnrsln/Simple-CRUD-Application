4f636f16b9f9d33c6a286f32c75403e48a43ca30
Gökhan Arslan <goekhan.arslan@outlook.de>
2025-03-05 15:58:21 +0100

    add unit tests for person list item

--- frontend/src/app/person-list-item/person-list-item.component.spec.ts
+++ frontend/src/app/person-list-item/person-list-item.component.spec.ts
@@ -1,27 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PersonListItemComponent } from './person-list-item.component';
 import { provideHttpClient } from '@angular/common/http';
+import { PersonService } from '../shared/service/person.service';
+import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 describe('PersonListItemComponent', () => {
   let component: PersonListItemComponent;
   let fixture: ComponentFixture<PersonListItemComponent>;
+  let personServiceSpy: jasmine.SpyObj<PersonService>;
 
   beforeEach(async () => {
+    personServiceSpy = jasmine.createSpyObj('PersonService', ['deletePerson']);
+
     await TestBed.configureTestingModule({
       imports: [PersonListItemComponent],
       providers: [
         provideHttpClient(),
+        { provide: PersonService, useValue: personServiceSpy },
+        { provide: ActivatedRoute, useValue: { params: of({ id: '1' }) } },
+    
       ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(PersonListItemComponent);
     component = fixture.componentInstance;
+    component.person = { id: '1', firstName: 'John Doe' , lastName: 'Doe', birthday: new Date('2000-01-01') };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call deletePerson and emit deleted event', () => {
+    spyOn(component.deleted, 'emit');
+    personServiceSpy.deletePerson.and.returnValue(of({}));
+
+    component.deletePerson('1');
+
+    expect(personServiceSpy.deletePerson).toHaveBeenCalledWith('1');
+    expect(component.deleted.emit).toHaveBeenCalledWith('1');
+  });
+
 });
