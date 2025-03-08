af3a2650c2aee2028bc6a407a685b936759e006e
Gökhan Arslan <goekhan.arslan@outlook.de>
2025-03-08 14:14:48 +0100

    add more unit tests for login

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { provideHttpClient } from '@angular/common/http';
 import { AuthService } from '../shared/service/auth.service';
-import { of } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -42,11 +42,25 @@ describe('LoginComponent', () => {
     expect(component.loginForm.get('password')?.value).toBe('Password');
   });
 
+  it('should not submit if form is invalid', () => {
+    component.loginForm.get('userName')?.setValue('');
+    component.onSubmit();
+    expect(authService.login).not.toHaveBeenCalled();
+  });
+
   it('should login on submit', () => {
     authService.login.and.returnValue(of({ token: 'test-token'}));
 
     component.onSubmit();
 
     expect(authService.login).toHaveBeenCalledWith('Username', 'Password');
+  });
+
+  it('should handle login error', () => {
+    authService.login.and.returnValue(throwError(() => 'Login failed'));
+
+    component.onSubmit();
+
+    expect(component.errorMessage).toBe('Login failed');
   });
 });
